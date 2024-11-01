f239f0a8966449a80ae5ada9b18bd991deb925e8
Selena <selena.oerding@gmail.com>
2024-11-01 15:45:09 +0100

    task7 complete

--- code/streatch-goal.js
+++ code/streatch-goal.js
@@ -11,22 +11,37 @@ const course = {
     lastName: "Jones",
   },
 };
-const numbers = [0, 15, -3, 9, null, 22, undefined, 8, NaN, false, 100];
+const numbers1 = [0, 15, -3, 9, null, 22, undefined, 8, NaN, false, 100];
 
 // Task 1: Object Manipulation
 // Use the spread operator to create a new object called `updatedStudent`, updating the age to 21
 // and adding a new property `major` with the value "Computer Science".
 // Log `updatedStudent` to the console.
+const updatedStudent = {
+  ...student,
+  age: 21,
+  major: "Computer Science",
+};
+
+console.log(updatedStudent);
 
 // Task 2: Nested Destructuring
 // Use nested destructuring to extract the firstName and lastName of the instructor from the `course` object
 // into variables `instructorFirstName` and `instructorLastName`.
 // Log `instructorFirstName` and `instructorLastName` to the console.
+const {
+  instructor: { firstName: instructorFirstName, lastName: instructorLastName },
+} = course;
 
 // Task 3: Filtering and Truthiness
 // Write a function called `filterValidNumbers` that filters out all falsy values and any negative numbers
 // from the `numbers` array.
 // Call `filterValidNumbers` with `numbers` and log the result to the console.
+function filterValidNumbers(arr) {
+  return arr.filter((num) => num > 0);
+}
+const validNumbers = filterValidNumbers(numbers1);
+console.log(validNumbers);
 
 // Task 4: Loop with Conditionals
 // Write a function called `analyzeStudentGrades` that takes an object representing a student
@@ -35,3 +50,19 @@ const numbers = [0, 15, -3, 9, null, 22, undefined, 8, NaN, false, 100];
 // If it's between 70 and 85, print "Good job, [Student Name]!".
 // If it's below 70, print "[Student Name], you need to improve.".
 // Call `analyzeStudentGrades` with the `updatedStudent` object.
+function analyzeStudentGrades(student) {
+  const { name, grades } = student;
+
+  const total = grades.reduce((sum, grade) => sum + grade, 0);
+  const average = total / grades.length;
+
+  if (average > 85) {
+    console.log(`Excellent performance, ${name}!`);
+  } else if (average >= 70) {
+    console.log(`Good job, ${name}!`);
+  } else {
+    console.log(`${name}, you need to improve.`);
+  }
+}
+
+analyzeStudentGrades(updatedStudent);
